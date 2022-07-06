f5a147e65ed793a3dfd05ba4ece44990565d80c4
odenigbo Godfrey <odenigbogodfrey@yahoo.com>
2022-07-06 01:47:08 +0100

    added github actions and env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DeliveryServiceModule } from './deliver-service/deliver-service.module';
-import { ListenerController } from './listener/listener.controller';
-import { DeliveryAgentController } from './delivery-agent/delivery-agent.controller';
-import { DeliveryAgentService } from './delivery-agent/delivery-agent.service';
 import { DeliveryAgentModule } from './delivery-agent/delivery-agent.module';
 import { ConfigModule } from '@nestjs/config';
 
@@ -14,7 +11,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot(),
     MongooseModule.forRoot(
       // 'mongodb://localhost:27017/delivery_service_db', 
-      'mongodb://162.0.230.238:27017/delivery_service_db',
+      `mongodb://${ process.env.MONGO_HOST || '162.0.230.238:27017'}/delivery_service_db`,
       {
     autoCreate: true
   }),
